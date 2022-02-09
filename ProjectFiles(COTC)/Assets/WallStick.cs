12987d59d15c7b235338c7164eb52abe60e5eb8a
kalebtaylor3 <taylorkaleb16@gmail.com>
2022-02-09 15:50:35 -0500

    yes

--- ProjectFiles(COTC)/Assets/WallStick.cs
+++ ProjectFiles(COTC)/Assets/WallStick.cs
@@ -8,15 +8,18 @@ public class WallStick : MonoBehaviour
     private Player2 player;
     private Rigidbody rb;
 
+    public bool horizontal;
     float y;
+    float x;
 
     bool climbing = false;
 
     private void Update()
     {
         y = Input.GetAxisRaw("VerticalArrow");
+        x = Input.GetAxisRaw("HorizontalArrow");
 
-        if(player.dimensionalController.currentDimension != Dimension.Dimensions.Yellow)
+        if (player.dimensionalController.currentDimension != Dimension.Dimensions.Yellow)
         {
             player.climbing = false;
             climbing = false;
@@ -25,16 +28,49 @@ public class WallStick : MonoBehaviour
         if(climbing)
         {
             rb.useGravity = false;
-            rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
-            float speedMod = y > 0 ? 0.35f : 1;
-            if(Input.GetKey(KeyCode.UpArrow))
-                player.transform.position += new Vector3(0, 3 * Time.deltaTime, 0);
-            if (Input.GetKey(KeyCode.DownArrow))
-                player.transform.position += new Vector3(0, -3 * Time.deltaTime, 0);
+            if (!horizontal)
+            {
+                rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+                float speedMod = y > 0 ? 0.35f : 1;
+                if (Input.GetKey(KeyCode.UpArrow))
+                    player.transform.position += new Vector3(0, 3 * Time.deltaTime, 0);
+                if (Input.GetKey(KeyCode.DownArrow))
+                    player.transform.position += new Vector3(0, -3 * Time.deltaTime, 0);
+            }
+            else
+            {
+                rb.velocity = new Vector3(0, 0, rb.velocity.z);
+                float speedMod = x > 0 ? 0.35f : 1;
+                if (Input.GetKey(KeyCode.RightArrow))
+                    player.transform.position += new Vector3(3 * Time.deltaTime, 0, 0);
+                if (Input.GetKey(KeyCode.LeftArrow))
+                    player.transform.position += new Vector3(-3 * Time.deltaTime, 0, 0);
+            }
         }
         else
         {
             rb.useGravity = true;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+
+            player = collision.gameObject.GetComponent<Player2>();
+            rb = collision.gameObject.GetComponent<Rigidbody>();
+
+            if (player != null)
+            {
+                if (player.dimensionalController.currentDimension == Dimension.Dimensions.Yellow)
+                {
+                    //if (horizontal)
+                        
+                    //else
+                        
+                }
+            }
         }
     }
 
