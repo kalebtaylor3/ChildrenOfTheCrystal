2a98752533a21be45823cf41d4888abf90e5e213
kalebtaylor3 <taylorkaleb16@gmail.com>
2022-02-13 15:28:43 -0500

    Bug Fix

--- ProjectFiles(COTC)/Assets/Game/Scripts/Player/Player2.cs
+++ ProjectFiles(COTC)/Assets/Game/Scripts/Player/Player2.cs
@@ -8,10 +8,30 @@ public class Player2 : PlayerMove
     private bool direction;
     public GameObject speedTrail;
 
-    public static event Action OnLeave;
+    //public static event Action OnLeave;
+    private Rigidbody rb;
+
+    float y;
+    float x;
+
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        WallStick.OnWall += ClimbWall;
+        WallStick.OffWall += OffWall;
+    }
 
     private void Update()
     {
+
+        y = Input.GetAxisRaw("VerticalArrow");
+        x = Input.GetAxisRaw("HorizontalArrow");
 
         float h = Input.GetAxisRaw("HorizontalArrow");
         float v = Input.GetAxisRaw("VerticalArrow");
@@ -30,7 +50,8 @@ public class Player2 : PlayerMove
 
         if(this.dimensionalController.currentDimension != Dimension.Dimensions.Yellow)
         {
-            OnLeave?.Invoke();
+            //OnLeave?.Invoke();
+            rb.useGravity = true;
         }
 
     }
@@ -62,5 +83,37 @@ public class Player2 : PlayerMove
     {
         this.accel = 50;
         speedTrail.SetActive(false);
+    }
+
+
+    void OffWall()
+    {
+        rb.useGravity = true;
+    }
+
+    void ClimbWall(bool horizontal)
+    {
+        rb.useGravity = false;
+
+        if (!horizontal)
+        {
+            rb.velocity = new Vector3(0, 0, 0);
+            float speedMod = y > 0 ? 0.35f : 1;
+            if (Input.GetKey(KeyCode.UpArrow))
+                this.transform.position += new Vector3(0, 3 * Time.deltaTime, 0);
+            if (Input.GetKey(KeyCode.DownArrow))
+                this.transform.position += new Vector3(0, -3 * Time.deltaTime, 0);
+
+        }
+
+        if (horizontal)
+        {
+            rb.velocity = new Vector3(0, 0, 0);
+            float speedMod = x > 0 ? 0.35f : 1;
+            if (Input.GetKey(KeyCode.RightArrow))
+                this.transform.position += new Vector3(3 * Time.deltaTime, 0, 0);
+            if (Input.GetKey(KeyCode.LeftArrow))
+                this.transform.position += new Vector3(-3 * Time.deltaTime, 0, 0);
+        }
     }
 }
--- ProjectFiles(COTC)/Assets/Game/Scripts/WallStick.cs
+++ ProjectFiles(COTC)/Assets/Game/Scripts/WallStick.cs
@@ -1,85 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class WallStick : MonoBehaviour
 {
-
-    private Player2 player;
-    private Rigidbody rb;
-
     public bool horizontal;
-    float y;
-    float x;
 
-    bool climbing = false;
 
-    private void OnEnable()
-    {
-        Player2.OnLeave += NotClimbing;
-    }
-
-    private void Update()
-    {
-
-        y = Input.GetAxisRaw("VerticalArrow");
-        x = Input.GetAxisRaw("HorizontalArrow");
-
-        Debug.Log("Climbing is: " + climbing);
-
-        if(climbing)
-        {
-            rb.useGravity = false;
-            if (!horizontal)
-            {
-                rb.velocity = new Vector3(0, 0, 0);
-                float speedMod = y > 0 ? 0.35f : 1;
-                if (Input.GetKey(KeyCode.UpArrow))
-                    player.transform.position += new Vector3(0, 3 * Time.deltaTime, 0);
-                if (Input.GetKey(KeyCode.DownArrow))
-                    player.transform.position += new Vector3(0, -3 * Time.deltaTime, 0);
-
-            }
-            
-            if(horizontal)
-            {
-                rb.velocity = new Vector3(0, 0, 0);
-                float speedMod = x > 0 ? 0.35f : 1;
-                if (Input.GetKey(KeyCode.RightArrow))
-                    player.transform.position += new Vector3(3 * Time.deltaTime, 0, 0);
-                if (Input.GetKey(KeyCode.LeftArrow))
-                    player.transform.position += new Vector3(-3 * Time.deltaTime, 0, 0);
-            }
-        }
-    }
-
-    void NotClimbing()
-    {
-        player.climbing = false;
-        climbing = false;
-        rb.useGravity = true;
-    }
+    public static event Action<bool> OnWall;
+    public static event Action OffWall;
 
     private void OnCollisionStay(Collision collision)
     {
         if(collision.gameObject.tag == "Player")
-        {
-            player = collision.gameObject.GetComponent<Player2>();
-            rb = collision.gameObject.GetComponent<Rigidbody>();
-            if (player != null)
-            {
-                if (player.dimensionalController.currentDimension == Dimension.Dimensions.Yellow)
-                {
-                    Debug.Log(climbing);
-                    player.climbing = true;
-                    climbing = true;
-                }
-            }
-        }
+            OnWall?.Invoke(horizontal);
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        NotClimbing();
+        OffWall?.Invoke();
     }
 }
