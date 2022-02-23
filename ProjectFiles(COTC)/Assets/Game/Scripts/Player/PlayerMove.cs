beec9ec20c0469dce08b71ad5f81d6971d2495ab
tydi4907 <tydi4907@gmail.com>
2022-02-23 00:52:58 -0500

    push

--- ProjectFiles(COTC)/Assets/Game/Scripts/Player/PlayerMove.cs
+++ ProjectFiles(COTC)/Assets/Game/Scripts/Player/PlayerMove.cs
@@ -166,7 +166,7 @@ public class PlayerMove : MonoBehaviour
 
 		foreach (Transform child in obj.transform)
 		{
-			Transform checker = transform.FindChild("PlayerChecker");
+			Transform checker = transform.Find("PlayerChecker");
 			if (null == child)
 			{
 				continue;
