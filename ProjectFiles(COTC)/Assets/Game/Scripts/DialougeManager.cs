bd43149028986efea7e02e03ffb546d54fe0b7fa
kalebtaylor3 <taylorkaleb16@gmail.com>
2022-04-22 16:51:54 -0400

    Update DialougeManager.cs

--- ProjectFiles(COTC)/Assets/Game/Scripts/DialougeManager.cs
+++ ProjectFiles(COTC)/Assets/Game/Scripts/DialougeManager.cs
@@ -25,8 +25,6 @@ public class DialougeManager : MonoBehaviour
 
     public static event Action onNext;
 
-    public AudioSource evilSource;
-
     // Start is called before the first frame update
     void Start()
     {
