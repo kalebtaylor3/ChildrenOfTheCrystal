8615f6b8aadc8e5703a821162967371e477655d4
kalebtaylor3 <taylorkaleb16@gmail.com>
2022-04-22 00:18:12 -0400

    camerashakeforend

--- ProjectFiles(COTC)/Assets/Game/Scripts/Environment/TriggerTimer.cs
+++ ProjectFiles(COTC)/Assets/Game/Scripts/Environment/TriggerTimer.cs
@@ -7,10 +7,16 @@ public class TriggerTimer : MonoBehaviour
 {
 
     [SerializeField] private UnityEvent trigger;
+    public Camera mainCam;
+
+    public float shakeInX = 0.5f;
+    public float shakeInY = 0.2f;
+
     // Start is called before the first frame update
 
     private void OnTriggerEnter(Collider other)
     {
         trigger.Invoke();
+        mainCam.GetComponent<CameraFollow>().Shake(shakeInX, shakeInY);
     }
 }
